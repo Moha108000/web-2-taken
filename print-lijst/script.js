90034bcd5c41d764e9e85d3749815447258e8836
Moha108000 <94456333+Moha108000@users.noreply.github.com>
2022-01-27 16:37:47 +0100

    lijst

---
+++ print-lijst/script.js
@@ -0,0 +1,11 @@
+var voetballer1 = {naam:"messi", leeftijd:"36jaarj", club:"barcelona"};
+
+var voetballer2 = {Naam:"ronaldo", leeftijd:"38jaar", club:"Manutd"};
+
+var voetballer3 = {naam: "neymar", leeftijd:"31jaar", club:"PSG"};
+
+var Lijstobjecten = [voetballer1, voetballer2, voetballer3]
+
+for (let object of Lijstobjecten) {
+    console.log(object);
+}
