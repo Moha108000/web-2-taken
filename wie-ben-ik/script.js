b092d795445870dd73c89a6519418738962a113c
Moha108000 <94456333+Moha108000@users.noreply.github.com>
2022-02-01 16:40:05 +0100

    bb

---
+++ wie-ben-ik/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+console.log('Mohamed oefening werkt');
+
+let h1Element = document .querySelector('.h1')
+
+console.log(h1Element);
+h1Element.style.backgroundColor = 'yellow' ;
+//
+
+let h1Element = document .querySelector('#Mohamed').style.textDecoration = "underline"
+
+console.log(h1Element);
+h1Element.style.backgroundColor = 'yellow' ;
+
+//.style.textDecoration = "underline";
+
+
