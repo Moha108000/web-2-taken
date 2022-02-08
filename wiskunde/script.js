82ab7a0ce5c1f27f358723ac9fea836d20f3838d
Moha108000 <94456333+Moha108000@users.noreply.github.com>
2022-02-08 16:11:22 +0100

    ff

--- wiskunde/script.js
+++ wiskunde/script.js
@@ -1,4 +1,11 @@
 let getal1 = prompt('geeft het eerste getal');
+
 let getal2 = prompt('geef he 2de getal');
-let som = getal1 + getal2;
-alert(som);
+
+let isGetal1KleinerDangetal2 = getal1 < getal2;
+
+if(isGetal1KleinerDangetal2){
+    alert('Getal 1 is kleiner dan getal 2')
+} else{
+    alert('Getal 1 is groter dan getal 2')
+}
