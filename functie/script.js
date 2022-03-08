6851b67812d37446e5f238c5774c9a7692218f76
Moha108000 <94456333+Moha108000@users.noreply.github.com>
2022-03-08 15:33:21 +0100

    k

---
+++ functie/script.js
@@ -0,0 +1,14 @@
+let voetballer1 = {naam:"messi", leeftijd:"36jaarj", club:"barcelona"};
+
+let voetballer2 = {Naam:"ronaldo", leeftijd:"38jaar", club:"Manutd"};
+
+let voetballer3 = {naam: "neymar", leeftijd:"31jaar", club:"PSG"};
+
+let lijstobjecten = [voetballer1, voetballer2, voetballer3]
+
+console.log(lijstobjecten.length);
+
+for (let i = 0; i < lijstobjecten.length; i++) {
+    console.log(lijstobjecten[i]);
+}
+ 
